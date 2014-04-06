96996ffe9e4c8eeeef2580c5e785865aacbdcad0
Payam Naderi <naderi.payam@gmail.com>
2014-04-06 19:12:18 +0430

    initial commit

---
+++ src/yimaLocalize/Module.php
@@ -0,0 +1,36 @@
+<?php
+namespace yimaLocalize;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements
+    ConfigProviderInterface,
+    AutoloaderProviderInterface
+{
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+}
